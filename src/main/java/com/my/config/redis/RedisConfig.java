d0eb4ff7d5092598d8aa6819001dead79f783d6e
dangwgcd <lyg49884004074833@163.com>
2021-03-07 15:32:31 +0800

    springboot--redis--Obj,bug修复

--- src/main/java/com/my/config/redis/RedisConfig.java
+++ src/main/java/com/my/config/redis/RedisConfig.java
@@ -1,21 +1,44 @@
 package com.my.config.redis;
 
 import com.my.entity.RedisEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
+ * 针对Redis中存储自定义对象RedisEntity
  * Created by wt on 2017/12/5.
  */
 @Configuration
 public class RedisConfig {
+    @Autowired
+    private RedisProperties redisProperties;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(redisProperties.getPool().getMaxActive());
+        jedisPoolConfig.setMaxWaitMillis(redisProperties.getPool().getMaxWait());
+        jedisPoolConfig.setMaxIdle(redisProperties.getPool().getMaxIdle());
+        jedisPoolConfig.setMinIdle(redisProperties.getPool().getMinIdle());
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+
+        jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
+        jedisConnectionFactory.setHostName(redisProperties.getHost());
+        jedisConnectionFactory.setPassword(redisProperties.getPassword());
+        jedisConnectionFactory.setPort(redisProperties.getPort());
+        jedisConnectionFactory.setDatabase(redisProperties.getDatabase());
+        jedisConnectionFactory.setTimeout(redisProperties.getTimeout());
+
+
+        return jedisConnectionFactory;
     }
 
     @Bean
@@ -23,7 +46,6 @@ public class RedisConfig {
         RedisTemplate<String, RedisEntity> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new RedisObjectSerializer());
 
         return template;
     }
